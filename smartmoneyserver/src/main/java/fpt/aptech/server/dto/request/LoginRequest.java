c17e9a0bfe091e8101a10676f13878aadfd6526f
Phat <ducphatdeveloper@gmail.com>
2026-01-26 11:56:37 +0700

    cập nhật dto lần 1 test tính năng của github

--- smartmoneyserver/src/main/java/fpt/aptech/server/dto/request/LoginRequest.java
+++ smartmoneyserver/src/main/java/fpt/aptech/server/dto/request/LoginRequest.java
@@ -1,5 +1,6 @@
 package fpt.aptech.server.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,9 @@ import lombok.Setter;
 @Getter
 @Data
 public class LoginRequest {
+    @NotBlank(message = "Tên đăng nhập không được để trống")
     private String email;
+    @NotBlank(message = "Mật khẩu không được để trống")
     private String password;
     private String deviceToken; // Để lưu vào tUserDevices
     private String deviceType;
