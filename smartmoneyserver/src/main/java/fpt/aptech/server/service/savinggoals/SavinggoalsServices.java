46cddf6a75347aaae0bff4bde6b7ce3d685055e8
TranMinhNhatk5 <tranminhnhat2005a@gmail.com>
2026-02-08 00:15:17 +0700

    e sửa các chức năng của savinggoals

--- smartmoneyserver/src/main/java/fpt/aptech/server/service/savinggoals/SavinggoalsServices.java
+++ smartmoneyserver/src/main/java/fpt/aptech/server/service/savinggoals/SavinggoalsServices.java
@@ -1,9 +1,7 @@
 package fpt.aptech.server.service.savinggoals;
 
 import fpt.aptech.server.dto.savinggoals.reponse.SavingGoalResponse;
-import fpt.aptech.server.dto.savinggoals.request.AddSavingMemberRequest;
 import fpt.aptech.server.dto.savinggoals.request.CreateSavingGoalRequest;
-import fpt.aptech.server.dto.savinggoals.request.DepositSavingRequest;
 import fpt.aptech.server.dto.savinggoals.request.UpdateSavingGoalRequest;
 
 import java.util.List;
@@ -15,11 +13,11 @@ public interface SavinggoalsServices {
 
     SavingGoalResponse updateSavingGoal(Integer id, UpdateSavingGoalRequest request);
 
+
+
+
     void deleteSavingGoal(Integer id, Integer accId);
 
     List<SavingGoalResponse> getSavingGoalsByAccount(Integer accId);
 
-    void addMember(Integer savingGoalId, Integer ownerId, AddSavingMemberRequest request);
-
-    SavingGoalResponse deposit(Integer savingGoalId, DepositSavingRequest request);
 }
