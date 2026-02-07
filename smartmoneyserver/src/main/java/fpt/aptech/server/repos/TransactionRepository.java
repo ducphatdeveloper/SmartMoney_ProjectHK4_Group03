7e5c7ace5b732728d19f1426260c925bf190dbad
TranMinhNhatk5 <tranminhnhat2005a@gmail.com>
2026-02-08 00:14:32 +0700

    em thêm mẫu 1 transaction repo

---
+++ smartmoneyserver/src/main/java/fpt/aptech/server/repos/TransactionRepository.java
@@ -0,0 +1,10 @@
+package fpt.aptech.server.repos;
+
+
+import fpt.aptech.server.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+}
