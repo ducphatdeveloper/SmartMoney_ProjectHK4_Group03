a82a9571c2ba11d621882433bfda1f77dda8c765
TranMinhNhatk5 <tranminhnhat2005a@gmail.com>
2026-02-08 00:13:23 +0700

    bổ sung dto savinggoals

--- smartmoneyserver/src/main/java/fpt/aptech/server/dto/savinggoals/request/UpdateSavingGoalRequest.java
+++ smartmoneyserver/src/main/java/fpt/aptech/server/dto/savinggoals/request/UpdateSavingGoalRequest.java
@@ -1,5 +1,7 @@
 package fpt.aptech.server.dto.savinggoals.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,10 +12,21 @@ import java.time.LocalDate;
 @Setter
 public class UpdateSavingGoalRequest {
 
+    @NotNull
+    private Integer accId;
+
     private String goalName;
+    private Integer categoryId;
+    private String currencyCode;
+
     private BigDecimal targetAmount;
     private LocalDate endDate;
     private String goalImageUrl;
+
     private Boolean notified;
     private Boolean reportable;
+
+    // số tiền muốn nạp thêm vào ví tiết kiệm
+    @Positive
+    private BigDecimal amount;
 }
