702878f0f2cd85c2aebfa73c11c28b83e3224387
Phát <ducphatdeveloper@gmail.com>
2026-02-08 12:03:11 +0700

    Update BudgetRepository.java
    Fixed

--- smartmoneyserver/src/main/java/fpt/aptech/server/repos/BudgetRepository.java
+++ smartmoneyserver/src/main/java/fpt/aptech/server/repos/BudgetRepository.java
@@ -1,4 +1,10 @@
-package fpt.aptech.server.repos;
+package fpt.aptech.server.repos.Budget;
 
-public interface BudgetRepository {
+import fpt.aptech.server.entity.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BudgetRepository extends JpaRepository<Budget , Integer> {
+
 }
